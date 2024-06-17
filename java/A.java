2bcac15a79c1457832bc379786f38da3ccd1471e
YuvarajUno <yuvarajuno@gmail.com>
2024-06-17 13:13:06 +0530

    Initial Commit

---
+++ java/A.java
@@ -0,0 +1,22 @@
+
+Class B
+{
+void add(int a, int b)
+{
+
+System.out.println(a+b);
+}
+
+}
+
+
+class A
+{
+public static void main(String[] args)
+{
+B a = new B();
+a.add(10,90);
+
+}
+
+}
---
+++ java/B.java
@@ -0,0 +1,13 @@
+public class B
+{
+void add(int a, int b)
+{
+System.out.println(a+b);
+}
+public static void main(String[] args) 
+{
+B b = new B();
+b.add(10,90);
+}
+}
+
